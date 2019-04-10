d09233617278f62689f0fbf9aeecafadddedbef0
shorok mousatafa <shorok_m@live.nl>
2019-04-10 10:45:57 +0200

    navigatie

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function () {
-  
+  jQuery('.icon').click(function() {
+    jQuery('.mobile-overlay').toggleClass('mobile-overlay-show');
+    jQuery('.fa-bars').toggleClass('fa-times');
+    if (jQuery('.mobile-overlay').hasClass('mobile-overlay-show')) {
+      jQuery('.fa-bars').addClass('openclass')
+    } else {
+      jQuery('.fa-bars').removeClass('openclass');
+    }
+  });
 });
